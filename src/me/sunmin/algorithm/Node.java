7ac1b7cb0559b1862453a91563666e82f7632909
study_sun <s6530085@hotmail.com>
2019-02-22 20:00:33 +0800

    implement 111-114

--- src/me/sunmin/algorithm/Node.java
+++ src/me/sunmin/algorithm/Node.java
@@ -1,5 +1,7 @@
 package me.sunmin.algorithm;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Node {
@@ -12,5 +14,35 @@ public class Node {
         val = _val;
         children = _children;
     }
+    
+    private static void p(int remain, List<Integer>l, List<List<Integer>>list) {
+//    	if (remain == 1) {
+//    		l.add(remain);
+//    		list.add(l);
+//    	}
+    	for (int i = 2; i <= remain; i++) {
+    		if (remain%i==0) {
+    			List<Integer> nl = new ArrayList<Integer>(l);
+    			nl.add(i);
+    			
+    			List<Integer> nnl = new ArrayList<Integer>(nl);
+    			nnl.add(remain/i);
+    			list.add(nnl);
+    			
+    			p(remain/i, nl, list);
+    		}
+    	}
+    }
+    
+    
+    public static void main(String[] args) {
+    	List<List<Integer>>list = new ArrayList<List<Integer>>();
+    	int remain = 8;
+//    	list.add(Arrays.asList(new Integer[] {1, remain}));
+//    	list.add(Arrays.asList(new Integer[] {remain, 1}));
+    	p(remain, new ArrayList<Integer>(), list);
+    }
+    
+    
     
 }
